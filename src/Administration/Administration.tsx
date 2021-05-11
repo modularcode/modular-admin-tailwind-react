576a7874701ea70800d1c2a748aae688b62afd03
Gevorg Harutyunyan <gevharut@gmail.com>
2021-05-12 01:23:37 +0400

    Administration layout improvement

--- src/Administration/Administration.tsx
+++ src/Administration/Administration.tsx
@@ -6,7 +6,13 @@ type AdministrationProps = {}
 const content = faker.lorem.paragraphs(50)
 
 const Administration: React.FC<AdministrationProps> = () => {
-  return <div className="px-14 py-8">Administration: {content}</div>
+  return (
+    <div className="px-14 py-8">
+      <div className="bg-white shadow-sm py-6 px-8 rounded">
+        Administration: {content}
+      </div>
+    </div>
+  )
 }
 
 export default Administration
