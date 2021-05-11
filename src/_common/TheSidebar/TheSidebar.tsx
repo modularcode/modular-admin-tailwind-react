e98156aa992c60e0094b1f41a01e64625af02660
Gevorg Harutyunyan <gevharut@gmail.com>
2021-05-12 01:44:53 +0400

    Sidebar UI adjustments

--- src/_common/TheSidebar/TheSidebar.tsx
+++ src/_common/TheSidebar/TheSidebar.tsx
@@ -10,9 +10,26 @@ const TheSidebar: React.FC<TheSidebarProps> = () => {
   return (
     <div className="TheSidebar absolute h-full w-full flex flex-col">
       <div className="h-20 flex items-center top-0 px-4">
-        <NavLink to="/">
-          <BaseLogo size={30} className="ml-10 mr-4 text-primary-500" />{' '}
-          <span className="font-semibold text-gray-600">ModularAdmin</span>
+        <NavLink to="/" className="flex items-center ">
+          <BaseLogo
+            size={30}
+            className="ml-10 mr-4 text-primary-500 align-middle"
+          />{' '}
+          <span className="font-bold  align-middle relative ">
+            Modular Admin
+            <span
+              className="
+                absolute bg-indigo-400 opacity-60 text-white rounded-full px-1 py-0 top-full -right-2
+                whitespace-nowrap
+                font-normal
+              "
+              style={{
+                fontSize: '.5em',
+              }}
+            >
+              Tailwind + React
+            </span>
+          </span>
         </NavLink>
       </div>
       <div className="SidebarNavContainer flex-1 overflow-y-auto ">
