e5049b53e1fea6a23ac38aee1818be6a2dd8b019
Gevorg Harutyunyan <gevharut@gmail.com>
2021-05-12 01:28:37 +0400

    Sidebar logo link

--- src/_common/TheSidebar/TheSidebar.tsx
+++ src/_common/TheSidebar/TheSidebar.tsx
@@ -1,4 +1,6 @@
 import React, { memo } from 'react'
+import { NavLink } from 'react-router-dom'
+
 import BaseLogo from '_common/BaseLogo'
 import SidebarNav from './SidebarNav'
 
@@ -8,8 +10,10 @@ const TheSidebar: React.FC<TheSidebarProps> = () => {
   return (
     <div className="TheSidebar absolute h-full w-full flex flex-col">
       <div className="h-20 flex items-center top-0 px-4">
-        <BaseLogo size={30} className="ml-10 mr-4 text-primary-500" />{' '}
-        ModularAdmin
+        <NavLink to="/">
+          <BaseLogo size={30} className="ml-10 mr-4 text-primary-500" />{' '}
+          <span className="font-semibold text-gray-600">ModularAdmin</span>
+        </NavLink>
       </div>
       <div className="SidebarNavContainer flex-1 overflow-y-auto ">
         <SidebarNav />
